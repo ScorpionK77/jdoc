a6077e6a89852369c7eca725c62e085972e98820
Валерий Косухин <valeriy.kosukhin@tmk-group.com>
2026-09-16 17:41:22 +0300

    Добавлены профалер, поправлен докер

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
